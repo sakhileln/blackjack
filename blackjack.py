a0b32a32ba7cf3d24fef3aefef0ce0edfbe793af
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-09 23:51:48 +0200

    Add gameplay logic for the player.

--- blackjack.py
+++ blackjack.py
@@ -16,7 +16,20 @@ def backjack() -> None:
     game_over = False
 
     while not game_over:
-        ...
+        display_hands(player_hand, dealer_hand)
+
+        if calculate_score(player_hand) == 21:
+            print("Blackjack! You win!")
+            return
+        elif calculate_score(player_hand) > 21:
+            print("You went over 21. You lose!")
+            return
+        
+        should_continue = input("Type 'y' to get another card. 'n' to pass")
+        if should_continue == 'y':
+            player_hand.append(deal_card())
+        else:
+            game_over = True
 
     while calculate_score(dealer_hand) < 17:
         dealer_hand.append(deal_card())
