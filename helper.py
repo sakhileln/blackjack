f3b49aa7301f97da7255c26d49db54446727087c
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-09 23:03:18 +0200

    Calculate score of given hand.

--- helper.py
+++ helper.py
@@ -19,3 +19,21 @@ def deal_card() -> int:
         2,3,4,5,6,7,8,9,10,10,10,10,11
     ]
     return choice(cards)
+
+
+def calculate_score(hand) -> int:
+    """
+    Calculate the score og given hand.
+
+    Parameters:
+        hand (list): Player hand
+    Return:
+        score (int): Total score of player hand
+    """
+    score = sum(hand)
+    # Adjust for aces if if score is over 21
+    while score > 21 and 11 in hand:
+        hand.remove(11)
+        hand.append(1)
+        score = sum(hand)
+    return score
